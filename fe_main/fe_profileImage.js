457ebfb1c3f2706a5b85a8bcfab5c881766784d6
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2024-07-03 17:57:37 +0900

    프로필 사진 변경 기능 추가

---
+++ fe_main/fe_profileImage.js
@@ -0,0 +1,16 @@
+const realUpload = document.querySelector(".real-upload");
+const upload = document.querySelector(".profile_img");
+
+function getImageFiles(e) {
+  const file = e.currentTarget.files[0];
+  const url = URL.createObjectURL(file);
+
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    upload.src = url;
+  };
+  reader.readAsDataURL(file);
+}
+
+upload.addEventListener("click", () => realUpload.click());
+realUpload.addEventListener("change", getImageFiles);
