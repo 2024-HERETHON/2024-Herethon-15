2caad797504f7e0c7b0e4c85a0906b3b56d7af0a
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2024-07-05 01:08:47 +0900

    input하면 채팅 입력되는 것만 남음

--- fe_dm/fe_dm.js
+++ fe_dm/fe_dm.js
@@ -1,3 +1,4 @@
+// 파트에 따라 프로필 파트 부분 색 변경
 const partTag = document.getElementsByClassName("part");
 
 for (let i = 0; i < partTag.length; i++) {
@@ -12,3 +13,34 @@ for (let i = 0; i < partTag.length; i++) {
   }
   partTag[i].style.backgroundColor = color;
 }
+
+// 채팅 리스트에 마지막으로 대화한 내용이 뜨게 하기
+const summarySpace = document.querySelector(".content");
+const lastText = document
+  .querySelector(".dm_content")
+  .lastElementChild.querySelector("span").lastElementChild.innerText;
+summarySpace.innerText = lastText;
+
+// 채팅에서 프로필 사진과 이름 뜨게 하기
+const me = document.getElementsByClassName("me");
+const you = document.getElementsByClassName("you");
+
+for (let i = 0; i < me.length; i++) {
+  const myImage = document.createElement("img");
+  myImage.src = "images/basic_profile.png";
+  me[i].appendChild(myImage);
+}
+
+for (let i = 0; i < you.length; i++) {
+  const yourImage = document.createElement("img");
+  yourImage.src = "images/basic_profile.png";
+  you[i].prepend(yourImage);
+
+  const span = you[i].querySelector("span");
+  const nameSpace = document.createElement("div");
+  const yourName = "김민지";
+  nameSpace.innerText = yourName;
+  span.prepend(nameSpace);
+}
+
+// 채팅치면 입력되게 하기
